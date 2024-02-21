0d59c09551add0ea183cc67dc517b4ea385e3329
uielPark <parkuiery@gmail.com>
2024-02-21 11:23:58 +0900

    feat :: MogakApp

--- app/src/main/java/com/example/mogak_android/MogakApp.kt
+++ app/src/main/java/com/example/mogak_android/MogakApp.kt
@@ -1,4 +1,19 @@
 package com.example.mogak_android
 
-class MogakApp {
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.mogak_android.navigation.NAVIGATION_AUTH
+import com.example.mogak_android.navigation.authNavigation
+
+@Composable
+internal fun MogakApp() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = NAVIGATION_AUTH,
+    ) {
+        authNavigation(navController = navController)
+    }
 }
