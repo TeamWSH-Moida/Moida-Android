88b9d4c021640f0dfd4f409abbdb0c17ee52cb5c
uielPark <parkuiery@gmail.com>
2024-02-21 11:24:07 +0900

    feat :: AuthNavigation

--- app/src/main/java/com/example/mogak_android/navigation/AuthNavigation.kt
+++ app/src/main/java/com/example/mogak_android/navigation/AuthNavigation.kt
@@ -1,4 +1,16 @@
 package com.example.mogak_android.navigation
 
-class AuthNavigation {
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.navigation
+
+const val NAVIGATION_AUTH = "auth"
+
+internal fun NavGraphBuilder.authNavigation(navController: NavController) {
+    navigation(
+        route = NAVIGATION_AUTH,
+        startDestination = "",
+    ) {
+
+    }
 }
