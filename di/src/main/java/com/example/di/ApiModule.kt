560057f5b17f6fa078c8fd60f6918b935816a693
audgns10 <s23065@gsm.hs.kr>
2024-02-23 01:58:57 +0900

    feat :: apiModule에 retrofit 객체 생성

--- di/src/main/java/com/example/di/ApiModule.kt
+++ di/src/main/java/com/example/di/ApiModule.kt
@@ -1,9 +1,29 @@
 package com.example.di
 
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
-val apiModule: Module
-    get() = module {
+private val gson = GsonBuilder().setLenient().create()
 
+val apiModule = module {
+
+    single {
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(HttpLoggingInterceptor())
+            .build()
+        Retrofit.Builder()
+            .client(okHttpClient)
+            //.baseUrl()
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
     }
+}
