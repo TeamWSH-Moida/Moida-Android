8c96daf280cd842c9c3ebb955c524eb122bf4b50
uielPark <parkuiery@gmail.com>
2024-02-27 15:05:00 +0900

    chore :: 라이브러리 추가

--- design-system/build.gradle.kts
+++ design-system/build.gradle.kts
@@ -34,9 +34,16 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation(Dependency.AndroidX.CORE_KTX)
+    implementation(Dependency.AndroidX.APPCOMPAT)
+    implementation(Dependency.Google.MATERIAL)
+
+    implementation(Dependency.Compose.COMPOSE)
+    implementation(Dependency.Compose.COMPOSE_FOUNDATION)
+    implementation(platform(Dependency.Compose.COMPOSE_BOM))
+    implementation(Dependency.Compose.COMPOSE_MATERIAL)
+    implementation(Dependency.Compose.COMPOSE_MATERIAL3)
+    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
